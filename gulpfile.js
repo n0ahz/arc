8630f71e9a3b0e1cf1a176cd47ebcc40d2a4cdd4
Md. Hasan-Uz-Zaman Rashid <hz.ce06@gmail.com>
2023-02-22 20:41:30 +0600

    added main sass compilers and css build task...

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
+const sass = require('gulp-sass')(require('node-sass'));
+
 
 function defaultTask(cb) {
     // place code for your default task here
     cb();
+}
+
+// Compile sass into CSS & auto-inject into browsers
+function buildCSS() {
+    return gulp.src("src/scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("dist/css"))
+        .pipe(browserSync.stream());
 }
 
 function serve() {
@@ -18,3 +28,4 @@ function serve() {
 
 exports.default = defaultTask;
 exports.serve = serve;
+exports.css = buildCSS;

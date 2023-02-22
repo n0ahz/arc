457961632fafcd4548f854cbb61684e28b1c9333
Md. Hasan-Uz-Zaman Rashid <hz.ce06@gmail.com>
2023-02-22 20:12:28 +0600

    browser-sync installed with initial serve task for serving the static htmls..

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,20 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+
 function defaultTask(cb) {
     // place code for your default task here
     cb();
 }
 
+function serve() {
+    browserSync.init({
+        server: "./src",
+        open: false,
+    });
+    gulp.watch("./src/*.html").on("change", reload);
+}
+
 
 exports.default = defaultTask;
+exports.serve = serve;
